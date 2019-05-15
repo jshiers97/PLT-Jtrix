0ba5ca2b1233a2c3d4e2b9c2c306a025c9b7cea9
azimdjuraev <azimdj5@gmail.com>
2019-05-14 23:47:30 -0400

    can print chars now using printc()

--- codegen.ml
+++ codegen.ml
@@ -116,6 +116,7 @@ let translate (globals, functions) =
     let builder = L.builder_at_end context (L.entry_block the_function) in
 
     let int_format_str = L.build_global_stringptr "%d\n" "fmt" builder
+    and char_format_str = L.build_global_stringptr "%c\n" "fmt" builder
     and float_format_str = L.build_global_stringptr "%g\n" "fmt" builder
     and new_line = L.build_global_stringptr "%s\n" "fmt" builder in
 
@@ -313,6 +314,9 @@ let translate (globals, functions) =
           | A.Not                  -> L.build_not) e' "tmp" builder
       | SCall ("print", [e]) | SCall ("printb", [e]) ->
 	  L.build_call printf_func [| int_format_str ; (expr builder e) |]
+	    "printf" builder
+      | SCall ("printc", [e]) ->
+	  L.build_call printf_func [| char_format_str ; (expr builder e) |]
 	    "printf" builder
       | SCall ("printbig", [e]) ->
 	  L.build_call printbig_func [| (expr builder e) |] "printbig" builder
